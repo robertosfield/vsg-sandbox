ea9000a0a7f896f744b6da898aa2f5b398918062
Robert Osfield <robert@openscenegraph.com>
2020-07-03 12:37:51 +0100

    Simplify using new Data::Layout ability to set the format

--- include/vsgsandbox/Utils.h
+++ include/vsgsandbox/Utils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vsg/core/Array.h>
 #include <vsg/core/Array2D.h>
 // Some useful functions
 
@@ -9,8 +10,6 @@ namespace vsgsandbox
     vsg::ref_ptr<vsg::Data> createArray(std::uint32_t width, std::uint32_t height, void* data,
                                         VkFormat format = VK_FORMAT_UNDEFINED)
     {
-        auto result = vsg::Array2D<EType>::create(width, height, static_cast<EType*>(data));
-        result->setFormat(format);
-        return result;
+        return vsg::Array2D<EType>::create(width, height, static_cast<EType*>(data), vsg::Data::Layout{format});
     }
 }
